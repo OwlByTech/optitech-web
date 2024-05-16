c0152811e7a6b57939571527e49cabb10ca95f80
CamiloArias12 <ariaspira13@gmail.com>
2024-05-16 10:36:26 +0000

    add nextauth config

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { signIn, useSession } from 'next-auth/react'
+import SplashScreen from '@/app/components/splash/Splash'
+
+export default function Login() {
+
+    return (
+        <></>
+    )
+}
