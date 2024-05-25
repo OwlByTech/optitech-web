ae4b7b2d41f5428ccff9d789c55348976d56fa01
CamiloArias12 <ariaspira13@gmail.com>
2024-05-25 07:47:17 -0500

    add enviromet config

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const { withStoreConfig } = require("./store-config")
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextEnv = require("@next/env")
+const path = require("path")
+const dotenv = require("dotenv")
+
+const envPath = path.join(__dirname, ".env")
+dotenv.config({
+    path: envPath,
+})
+const nextConfig = withStoreConfig({
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // TODO: Fix all typescript errors
+        ignoreBuildErrors: true,
+    },
+    env: {
+        AUTH_SECRET: process.env.AUTH_SECRET,
+    },
+    reactStrictMode: true,
+    transpilePackages: ['next-auth'],
+})
+
+module.exports = nextConfig
