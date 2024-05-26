bc51c2ca3fca787a7941cfb7feca3baa8038f560
CamiloArias12 <ariaspira13@gmail.com>
2024-05-26 10:34:49 -0500

    add session config

--- src/modules/auth/templates/login.tsx
+++ src/modules/auth/templates/login.tsx
@@ -1,16 +1,14 @@
 "use client"
 import { Input } from '@/modules/common/components/input'
 import { signIn, useSession } from "next-auth/react";
-import { useFormState } from 'react-dom';
-import { loginUser } from '../actions';
 import { SubmitButton } from '@/modules/common/components/submit-button';
 import { InputPassword } from '@/modules/common/components/input-password';
 import { useRouter } from 'next/navigation';
+import { Button } from '@/modules/common/components/button';
 
 export default function Login() {
     const session = useSession()
     const user = session.data?.user
-    const [result, formAction] = useFormState(loginUser, { state: false })
     const route = useRouter()
     if (session.status === 'authenticated') {
         route.push("/")
@@ -21,19 +19,27 @@ export default function Login() {
         <>
             {session.status === 'unauthenticated' &&
                 <div className='w-screen h-screen flex flex-col items-center justify-center'>
-                    <form onSubmit={async (formData) => {
-                        await new Promise((resolve) => { setTimeout(resolve, 3000) })
-                        try {
-                            const response = await signIn("credentials", { email: "demo@demo.com", password: "demo" });
-                        } catch (e) {
-                            console.log(e)
-
-
-                        }
-                    }} className=' flex flex-col  gap-4 min-w-80 '>
+                    <form className=' flex flex-col  gap-4 min-w-80 '>
                         <Input label='Correo' name="email" required type='email' />
                         <InputPassword label='Contrasena' name="password" required />
-                        <SubmitButton >Aceptar</SubmitButton>
+                        <Button onClick={async () => {
+                            await new Promise((resolve) => { setTimeout(resolve, 3000) })
+                            try {
+                                const response = await signIn('credentials', {
+                                    email: "demo@demo.com",
+                                    password: "demo",
+                                    redirect: false,
+                                    callbackUrl: '/'
+                                })
+                                console.log(response)
+                            } catch (e) {
+                                console.log(e)
+
+
+                            }
+
+                        }}>Aceptar</Button>
+
                     </form>
                 </div>
             }
