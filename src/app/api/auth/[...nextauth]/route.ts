ee991a93d12e3ae1cc5342332ca0ea40f05c408c
CamiloArias12 <ariaspira13@gmail.com>
2024-06-02 18:00:38 -0500

    update auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,2 @@
-import authOptions from './options'
-import NextAuth from 'next-auth/next'
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+import { handlers } from "@/auth"
+export const { GET, POST } = handlers
--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod'
-export const { auth, signIn, signOut } = NextAuth({
+export const { auth, handlers, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
