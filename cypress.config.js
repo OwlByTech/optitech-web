e8bff3feb85f239d4d2b586fc1d0f4d18457f49e
CamiloArias12 <ariaspira13@gmail.com>
2024-06-04 13:18:23 -0500

    feat: add config env cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require('cypress')
+const path = require("path")
+const dotenv = require("dotenv")
 
+const envPath = path.join(__dirname, ".env")
+dotenv.config({
+    path: envPath,
+})
 module.exports = defineConfig({
+    env: {
+        API_URL: process.env.API_URL
+    },
     component: {
         devServer: {
             framework: 'next',
--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,7 +1,7 @@
-describe("Login test", async () => {
 
+describe("Login test", async () => {
     it("visit login page", async () => {
-        cy.visit("http://localhost:3001/login")
+        cy.visit(Cypress.env('API_URL'))
         cy.get('input[name="email"]').type("optitech@optitech.com")
         cy.get('input[name="password"]').type("optitech")
         cy.get('button[type="submit"]').click()
