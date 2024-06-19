3754d10bd726a07cec6106b32464679407e2e0b9
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-06-19 10:46:21 -0500

    fix: change english to spanish

--- src/modules/auth/templates/principal.tsx
+++ src/modules/auth/templates/principal.tsx
@@ -5,23 +5,23 @@ import Link from "next/link";
 export default function Principal() {
   return (
     <section className="flex flex-col items-center justify-center h-screen w-full">
-      <div className="flex flex-col justify-center p-10 rounded-3xl gap-3 border border-black h-60 w-60">
+      <div className="flex flex-col justify-center p-10 rounded-3xl gap-3 border border-black h-60 w-64">
         <div className="flex flex-col">
           <h1 className="text-2xl">OptiTech</h1>
-          <p>Welcome back</p>
+          <p>Bienvenido de nuevo</p>
         </div>
         <div className="flex  justify-between">
           <Link
             href="/login"
             className="bg-black text-white p-1 border border-black rounded-md hover:bg-white hover:text-black text-xs"
           >
-            Sign In
+            Iniciar sesión
           </Link>
           <Link
             href="/signUp"
             className="p-1 border border-black rounded-md hover:bg-black hover:text-white text-xs"
           >
-            Register
+            Registrarse
           </Link>
         </div>
       </div>
