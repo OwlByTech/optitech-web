46d8b98d163ec00b7671e359d1597d7687902fbf
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-06-22 22:33:10 -0500

    refactor: refactor de component button and templates

--- src/modules/common/components/button/index.tsx
+++ src/modules/common/components/button/index.tsx
@@ -1,20 +1,31 @@
 import { Button as ButtonNext, ButtonProps } from "@nextui-org/react";
+import Link from "next/link";
+
+interface CustomButtonProps extends ButtonProps {
+  href?: string;
+}
 
 export function Button({
   className,
   title,
   type,
   children,
+  href,
   ...props
-}: ButtonProps) {
-  return (
-    <ButtonNext
-      className={`text-white bg-black ${className}`}
-      radius="none"
-      type={type}
-      {...props}
-    >
+}: CustomButtonProps) {
+  const buttonElement = (
+    <ButtonNext className={className} radius="none" type={type} {...props}>
       {children}
     </ButtonNext>
   );
+
+  if (href) {
+    return (
+      <Link href={href} passHref>
+        {buttonElement}
+      </Link>
+    );
+  }
+
+  return buttonElement;
 }
