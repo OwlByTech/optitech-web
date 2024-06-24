cf4d7bcc698430b113260259c6a5b86505570ec8
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-06-24 15:38:23 -0500

    feat: add signp steps pages

---
+++ src/app/signUp/stepTwo/page.tsx
@@ -0,0 +1,9 @@
+import Step2 from "@/modules/auth/templates/register-step-2";
+
+export const metadata = {
+    title: "Registro - Paso 2",
+  };
+
+export default function Page() {
+    return <Step2/>;
+  }
