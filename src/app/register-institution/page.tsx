0d4aea5bf05ff5495081c225524e10a93ecf0746
CamiloArias12 <ariaspira13@gmail.com>
2024-06-29 17:23:42 -0500

    feat :add context register-institution

--- src/app/register-institution/page.tsx
+++
@@ -1,6 +0,0 @@
-import AddServices from "@/modules/auth/templates/add-services";
-import RegisterInstitution from "@/modules/auth/templates/register-institution";
-
-export default function Page() {
-  return <AddServices />;
-}
