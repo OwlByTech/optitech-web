5e69d7cf4667fc6fa43edeac8e368f0b74c5ab70
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-07-16 14:54:38 -0500

    style: add mobil style initial of configuration button

--- src/modules/dashboard/templates/routes.tsx
+++ src/modules/dashboard/templates/routes.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { usePathname } from "next/navigation";
-import { RoutesSidebar } from "../components/routes-sidebar";
-import { routesSidebarDown, routesSidebarUp } from "../types";
+import { useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
+import { usePathname } from "next/navigation";
+import { routesSidebarDown, routesSidebarUp } from "../types";
+import { RoutesSidebar } from "../components/routes-sidebar";
 
 interface RoutesProps {
   closeRoutes?: () => void;
@@ -10,17 +11,23 @@ interface RoutesProps {
 
 export function Routes({ closeRoutes }: RoutesProps) {
   const path = usePathname();
+  const [showSettings, setShowSettings] = useState(false);
+
+  const toggleSettings = () => {
+    setShowSettings(!showSettings);
+  };
 
   return (
     <div className="h-full flex flex-col pb-16 pt-10 md:pt-14 justify-between">
       <div
-        className="flex flex-row items-center cursor-pointer gap-3 px-2"
+        className="flex flex-row items-center cursor-pointer gap-3 px-2 md:hidden"
         onClick={closeRoutes}
       >
-        <FiArrowLeft className="md:hidden w-6 h-6 cursor-pointer" />
-        <p className="text-sm">Atras</p>
+        <FiArrowLeft className="w-6 h-6 cursor-pointer" />
+        <p className="text-sm">Atrás</p>
       </div>
-      <div>
+
+      <div className="md:flex md:flex-col md:flex-grow md:justify-between">
         <RoutesSidebar routes={routesSidebarUp} path={path} />
         <RoutesSidebar routes={routesSidebarDown} path={path} />
       </div>
--- src/modules/settings/components/settings/index.tsx
+++ src/modules/settings/components/settings/index.tsx
@@ -7,16 +7,16 @@ export function Settings({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="flex flex-grow gap-4 h-full rounded-3xl">
-      <div className="flex flex-col justify-between bg-white h-full w-3/12 border rounded-3xl p-5 2xl:w-2/12">
+    <section className="flex md:flex-row md:flex-grow flex-col gap-4 h-full w-auto rounded-3xl">
+      <div className="flex flex-col justify-between bg-white h-full  md:w-3/12 border rounded-3xl p-5 md:2xl:w-2/12">
         <div className="flex flex-col gap-2">
           <RoutesSettings />
         </div>
         <div>
-          <SignOut className="bg-white rounded-xl hover:bg-gray-200" />
+          <SignOut className="bg-white rounded-xl border border-black text-black hover:bg-gray-200" />
         </div>
       </div>
-      <div className="flex flex-col justify-between bg-white h-full w-9/12 border rounded-3xl p-5">
+      <div className="flex flex-col justify-between bg-white h-auto md:w-9/12 border rounded-3xl p-5">
         {children}
       </div>
     </section>
