dcdbe37214ba94bb151d0e730b4e5382e308d4f9
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-07-16 12:32:31 -0500

    style: add mobile to routes of sidebar

--- src/modules/dashboard/templates/routes.tsx
+++ src/modules/dashboard/templates/routes.tsx
@@ -1,16 +1,29 @@
 "use client";
-"use client";
 import { usePathname } from "next/navigation";
 import { RoutesSidebar } from "../components/routes-sidebar";
 import { routesSidebarDown, routesSidebarUp } from "../types";
+import { FiArrowLeft } from "react-icons/fi";
 
-export function Routes() {
-    const path = usePathname();
+interface RoutesProps {
+  closeRoutes?: () => void;
+}
 
-    return (
-        <div className="h-full flex flex-col pt-14 justify-between">
-            <RoutesSidebar routes={routesSidebarUp} path={path} />
-            <RoutesSidebar routes={routesSidebarDown} path={path} />
-        </div>
-    );
+export function Routes({ closeRoutes }: RoutesProps) {
+  const path = usePathname();
+
+  return (
+    <div className="h-full flex flex-col pb-16 pt-10 md:pt-14 justify-between">
+      <div
+        className="flex flex-row items-center cursor-pointer gap-3 px-2"
+        onClick={closeRoutes}
+      >
+        <FiArrowLeft className="md:hidden w-6 h-6 cursor-pointer" />
+        <p className="text-sm">Atras</p>
+      </div>
+      <div>
+        <RoutesSidebar routes={routesSidebarUp} path={path} />
+        <RoutesSidebar routes={routesSidebarDown} path={path} />
+      </div>
+    </div>
+  );
 }
--- src/modules/dashboard/templates/topbar.tsx
+++ src/modules/dashboard/templates/topbar.tsx
@@ -1,13 +1,44 @@
 "use client";
+import { useState, useEffect, useRef } from "react";
 import { Button } from "@/modules/common/components/button";
 import { FiInfo, FiMenu, FiPauseCircle } from "react-icons/fi";
 import { TimeTopBar } from "../components/time-top-bar";
+import { Routes } from "./routes";
 
 export function TopBar() {
+  const [isRoutesVisible, setIsRoutesVisible] = useState(false);
+  const routesRef = useRef(null);
+
+  const toggleRoutes = () => {
+    setIsRoutesVisible(!isRoutesVisible);
+  };
+
+  const closeRoutes = () => {
+    setIsRoutesVisible(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (routesRef.current && event.target instanceof Node) {
+        if (!routesRef.current.contains(event.target as Node)) {
+          setIsRoutesVisible(false);
+        }
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [routesRef]);
+
   return (
     <div className="flex h-20 flex-row p-5 md:bg-transparent bg-white items-center justify-between w-screen md:w-full gap-4">
       <TimeTopBar />
-      <FiMenu className="w-6 h-6 md:hidden cursor-pointer" />
+      <FiMenu
+        className="w-6 h-6 md:hidden cursor-pointer"
+        onClick={toggleRoutes}
+      />
 
       <div className="flex flex-row w-auto md:gap-2 ">
         <Button
@@ -33,6 +64,14 @@ export function TopBar() {
           src="/profile.png"
         />
       </div>
+      {isRoutesVisible && (
+        <div
+          ref={routesRef}
+          className="fixed top-0 left-0 w-[280px] h-full bg-white z-50"
+        >
+          <Routes closeRoutes={closeRoutes} />
+        </div>
+      )}
     </div>
   );
 }
