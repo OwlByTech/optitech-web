e3496b01de77f03b84b885cbd47118935acd0ca9
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-07-16 11:54:39 -0500

    style: add mobile to root page

--- src/modules/auth/templates/principal.tsx
+++ src/modules/auth/templates/principal.tsx
@@ -4,45 +4,45 @@ import { Button } from "@/modules/common/components/button";
 import { ROUTES_AUTH } from "../types/auth";
 
 export type PrincipalProps = {
-    // TODO: Add type to clientInfo
-    clientInfo: any;
+  // TODO: Add type to clientInfo
+  clientInfo: any;
 };
 
 export default function Principal(props: PrincipalProps) {
-    return (
-        <section className="flex flex-col items-center justify-center h-screen w-full">
-            <div className="flex flex-col justify-center p-10 rounded-3xl gap-3 border border-black h-60 w-64">
-                <div className="flex flex-col">
-                    <h1 className="text-2xl">OptiTech</h1>
-                    <p>Bienvenido de nuevo</p>
-                </div>
-                {props.clientInfo ? (
-                    <>
-                        <div className="flex flex-col font-bold justify-center">
-                            <p>{props.clientInfo.GivenName}</p>
-                            <p>{props.clientInfo.Id}</p>
-                            <p>{props.clientInfo.Surname}</p>
-                            <p>{props.clientInfo.Email}</p>
-                        </div>
-                        <SignOut />
-                    </>
-                ) : (
-                    <div className="flex justify-between">
-                        <Button
-                            className="bg-black text-white p-1 border border-black rounded-md hover:bg-white hover:text-black text-xs"
-                            href={ROUTES_AUTH.LOGIN}
-                        >
-                            Iniciar sesión
-                        </Button>
-                        <Button
-                            className="bg-white p-1 border border-black rounded-md hover:bg-black hover:text-white text-xs"
-                            href={ROUTES_AUTH.SIGN_UP}
-                        >
-                            Registrarse
-                        </Button>
-                    </div>
-                )}
+  return (
+    <section className="flex flex-col items-center justify-center h-screen w-full">
+      <div className="flex flex-col justify-center p-10 rounded-3xl gap-3 border border-black h-60 w-64">
+        <div className="flex flex-col">
+          <h1 className="text-2xl">OptiTech</h1>
+          <p>Bienvenido de nuevo</p>
+        </div>
+        {props.clientInfo ? (
+          <>
+            <div className="flex flex-col font-bold justify-center">
+              <p>{props.clientInfo.GivenName}</p>
+              <p>{props.clientInfo.Id}</p>
+              <p>{props.clientInfo.Surname}</p>
+              <p>{props.clientInfo.Email}</p>
             </div>
-        </section>
-    );
+            <SignOut />
+          </>
+        ) : (
+          <div className="flex justify-between">
+            <Button
+              className="bg-black text-white p-1 border border-black rounded-md hover:bg-white hover:text-black text-xs"
+              href={ROUTES_AUTH.LOGIN}
+            >
+              Iniciar sesión
+            </Button>
+            <Button
+              className="bg-white p-1 border border-black rounded-md text-black hover:bg-black hover:text-white text-xs"
+              href={ROUTES_AUTH.SIGN_UP}
+            >
+              Registrarse
+            </Button>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
--- src/modules/dashboard/templates/topbar.tsx
+++ src/modules/dashboard/templates/topbar.tsx
@@ -4,36 +4,35 @@ import { FiInfo, FiMenu, FiPauseCircle } from "react-icons/fi";
 import { TimeTopBar } from "../components/time-top-bar";
 
 export function TopBar() {
+  return (
+    <div className="flex h-20 flex-row p-5 md:bg-transparent bg-white items-center justify-between w-screen md:w-full gap-4">
+      <TimeTopBar />
+      <FiMenu className="w-6 h-6 md:hidden cursor-pointer" />
 
-    return (
-        <div className="flex h-20 flex-row p-5 md:bg-transparent bg-white items-center justify-between w-screen md:w-full gap-4">
-            <TimeTopBar />
-            <FiMenu className="w-6 h-6 md:hidden cursor-pointer" />
-
-            <div className="flex flex-row w-auto md:gap-2 ">
-                <Button
-                    className="bg-white md:bg-gray-200 text-xs text-black h-10 md:w-32"
-                    isIconOnly
-                    radius="md"
-                    size="md"
-                    startContent={<FiPauseCircle className="md:h-5 md:w-5 w-6 h-6" />}
-                >
-                    <span className="md:pl-2 hidden md:block">Pendientes</span>
-                </Button>
-                <Button
-                    className="bg-white md:bg-gray-200 text-xs text-black h-10 md:w-32"
-                    isIconOnly
-                    radius="md"
-                    size="md"
-                    startContent={<FiInfo className="md:h-5 md:w-5 w-6 h-6" />}
-                >
-                    <span className="md:pl-2 hidden md:block">Notificaciones</span>
-                </Button>
-                <img
-                    className="md:hidden bg-white md:bg-gray-200 text-xs text-black h-10 w-10 md:w-32"
-                    src="/profile.png"
-                />
-            </div>
-        </div>
-    );
+      <div className="flex flex-row w-auto md:gap-2 ">
+        <Button
+          className="bg-white md:bg-gray-200 text-xs text-black h-10 md:w-32"
+          isIconOnly
+          radius="md"
+          size="md"
+          startContent={<FiPauseCircle className="md:h-5 md:w-5 w-6 h-6" />}
+        >
+          <span className="md:pl-2 hidden md:block">Pendientes</span>
+        </Button>
+        <Button
+          className="bg-white md:bg-gray-200 text-xs text-black h-10 md:w-32"
+          isIconOnly
+          radius="md"
+          size="md"
+          startContent={<FiInfo className="md:h-5 md:w-5 w-6 h-6" />}
+        >
+          <span className="md:pl-2 hidden md:block">Notificaciones</span>
+        </Button>
+        <img
+          className="md:hidden bg-white md:bg-gray-200 text-xs text-black h-10 w-10 md:w-32"
+          src="/profile.png"
+        />
+      </div>
+    </div>
+  );
 }
