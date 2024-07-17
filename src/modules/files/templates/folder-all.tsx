e480e9fc0972ac5c395976026b0aa9a7f52e71c6
camilo-camargo <camilocamargo49@gmail.com>
2024-07-17 12:22:42 -0500

    feat: add storage to context menu items

--- src/modules/files/templates/folder-all.tsx
+++ src/modules/files/templates/folder-all.tsx
@@ -3,114 +3,139 @@ import { Directory, File } from "../types";
 import { useAtom } from "jotai";
 import { directoryRoute, folderLayout } from "../context";
 import {
-    FolderDocumentOptions,
-    OptionComponentProps,
+  FolderDocumentOptions,
+  OptionComponentProps,
 } from "../components/folder-document-options";
 import { useEffect, useState } from "react";
+import { contextMenuStorage } from "@/modules/global/context-menu/context";
 
 export type DocumentDirectoryType = "document" | "directory";
 
 type OptionStateType = {
-    type: DocumentDirectoryType;
-    index: number;
-    component: React.ComponentType<OptionComponentProps>;
-    value: Directory | File;
+  type: DocumentDirectoryType;
+  index: number;
+  component: React.ComponentType<OptionComponentProps>;
+  value: Directory | File;
 } | null;
 
 type DocumentDiretoryType = "document" | "directory";
 type IsOpenOptionsType = {
-    type: DocumentDiretoryType;
-    index: number;
+  type: DocumentDiretoryType;
+  index: number;
 };
 
 export type FolderAllProps = {
-    // TODO: Add types from the services.
-    directory: Directory;
-    routeDirectory: Directory[];
+  // TODO: Add types from the services.
+  directory: Directory;
+  routeDirectory: Directory[];
 };
 
+const CONTEXT_MENU_ATTRIBUTE = "folder-all";
+
 export function FolderAll(props: FolderAllProps) {
-    const [layout, setLayout] = useAtom(folderLayout);
-    const [isOpenOptions, setIsOpenOptions] = useState<IsOpenOptionsType | null>(
-        null
-    );
-    const [optionState, setOptionState] = useState<OptionStateType>();
-    const [_, setDirectories] = useAtom(directoryRoute);
+  const [layout, setLayout] = useAtom(folderLayout);
+  const [contextMenuItems, setContextMenuItems] = useAtom(contextMenuStorage);
+  const [isOpenOptions, setIsOpenOptions] = useState<IsOpenOptionsType | null>(
+    null
+  );
+  const [optionState, setOptionState] = useState<OptionStateType>();
+  const [_, setDirectories] = useAtom(directoryRoute);
 
-    useEffect(() => {
-        setDirectories(props.routeDirectory);
-    }, [props.routeDirectory]);
+  useEffect(() => {
+    setContextMenuItems([
+      {
+        attribute: CONTEXT_MENU_ATTRIBUTE,
+        items: [
+          {
+            key: "new-directory",
+            title: "Crear carpeta",
+            handler: () => {
+              console.log(props.directory);
+            },
+          },
+        ],
+      },
+    ]);
+    console.log("settled");
+  }, []);
 
-    const onOpenOptions = (type: DocumentDiretoryType, index: number) => {
-        if (type === isOpenOptions?.type && index === index) {
-            setIsOpenOptions(null);
-        } else {
-            setIsOpenOptions({ type, index });
+  useEffect(() => {
+    setDirectories(props.routeDirectory);
+  }, [props.routeDirectory]);
+
+  const onOpenOptions = (type: DocumentDiretoryType, index: number) => {
+    if (type === isOpenOptions?.type && index === index) {
+      setIsOpenOptions(null);
+    } else {
+      setIsOpenOptions({ type, index });
+    }
+  };
+
+  const isOpenOptionsHandler = (type: DocumentDiretoryType, index: number) => {
+    return isOpenOptions?.type === type && isOpenOptions.index === index;
+  };
+
+  const onSelectOption = (
+    type: DocumentDiretoryType,
+    index: number,
+    component: React.ComponentType<OptionComponentProps>,
+    value: Directory | File
+  ) => {
+    setOptionState({ type, index, component, value });
+  };
+
+  return (
+    <div
+      data-contextmenu={CONTEXT_MENU_ATTRIBUTE}
+      className="h-full p-4 overflow-auto"
+    >
+      <div
+        className={
+          layout === "grid" ? "grid grid-cols-4 gap-4" : "flex flex-col gap-4"
         }
-    };
+      >
+        {optionState && (
+          <optionState.component
+            value={optionState.value}
+            onClose={() => setOptionState(null)}
+          />
+        )}
 
-    const isOpenOptionsHandler = (type: DocumentDiretoryType, index: number) => {
-        return isOpenOptions?.type === type && isOpenOptions.index === index;
-    };
-
-    const onSelectOption = (
-        type: DocumentDiretoryType,
-        index: number,
-        component: React.ComponentType<OptionComponentProps>,
-        value: Directory | File
-    ) => {
-        setOptionState({ type, index, component, value });
-    };
-
-    return (
-        <div className="h-full p-4 overflow-auto">
-            <div
-                className={
-                    layout === "grid" ? "grid grid-cols-4 gap-4" : "flex flex-col gap-4"
-                }
-            >
-                {optionState && (
-                    <optionState.component
-                        value={optionState.value}
-                        onClose={() => setOptionState(null)}
-                    />
-                )}
-
-                {/* {props.directory.parentId !== 0 && (
+        {/* {props.directory.parentId !== 0 && (
           <FolderView
             directory={{ id: props.directory.parentId, name: "..." }}
           />
         )} */}
 
-                {props.directory?.directory?.map((value, index) => (
-                    <FolderDocumentOptions
-                        layout={layout}
-                        key={index}
-                        onOpenOptions={() => onOpenOptions("directory", index)}
-                        isOpenOptions={isOpenOptionsHandler("directory", index)}
-                        onSelectOption={(component) =>
-                            onSelectOption("document", index, component, value)
-                        }
-                        onClosedOption={() => setIsOpenOptions(null)}
-                        type="directory"
-                        value={value}
-                    />
-                ))}
-                {props.directory?.document?.map((value, index) => (
-                    <FolderDocumentOptions
-                        layout={layout}
-                        key={index}
-                        onOpenOptions={() => onOpenOptions("document", index)}
-                        isOpenOptions={isOpenOptionsHandler("document", index)}
-                        onClosedOption={() => setIsOpenOptions(null)}
-                        onSelectOption={(component) =>
-                            onSelectOption("document", index, component, value)
-                        }
-                        type="document"
-                        value={value}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+        {props.directory?.directory?.map((value, index) => (
+          <FolderDocumentOptions
+            layout={layout}
+            key={index}
+            onOpenOptions={() => onOpenOptions("directory", index)}
+            isOpenOptions={isOpenOptionsHandler("directory", index)}
+            onSelectOption={(component) =>
+              onSelectOption("document", index, component, value)
+            }
+            onClosedOption={() => setIsOpenOptions(null)}
+            type="directory"
+            value={value}
+          />
+        ))}
+        {props.directory?.document?.map((value, index) => (
+          <FolderDocumentOptions
+            layout={layout}
+            key={index}
+            onOpenOptions={() => onOpenOptions("document", index)}
+            isOpenOptions={isOpenOptionsHandler("document", index)}
+            onClosedOption={() => setIsOpenOptions(null)}
+            onSelectOption={(component) =>
+              onSelectOption("document", index, component, value)
+            }
+            type="document"
+            value={value}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
