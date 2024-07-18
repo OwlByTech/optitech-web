f0d7181ad34ebeb14c195f929471241cc8f8d517
CamiloArias12 <127451734+CamiloArias12@users.noreply.github.com>
2024-07-18 11:35:32 -0500

    Update src/modules/files/components/file-view/index.tsx
    Co-authored-by: Camilo Andres Camargo Castañeda <camilocamargo49@gmail.com>

--- src/modules/files/components/file-view/index.tsx
+++ src/modules/files/components/file-view/index.tsx
@@ -8,10 +8,8 @@ type Props = {
 }
 
 export function FileView({ document }: Props) {
-
     return (
         <LinkRef
-
             href={`${ROUTES_SIDEBAR.FILES}/document/${document?.id}`}
             className={`hover:bg-gray-50 flex flex-col items-center justify-between h-24 w-24 rounded-lg bg-white text-black font-light  p-2 text-sm gap-2 border  `}>
 
@@ -21,5 +19,4 @@ export function FileView({ document }: Props) {
             </span>
         </LinkRef>
     )
-
 }
