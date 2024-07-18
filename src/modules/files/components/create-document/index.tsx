49bf1354406fd2aaef060bebc994d1c47f268a64
CamiloArias12 <127451734+CamiloArias12@users.noreply.github.com>
2024-07-18 10:55:38 -0500

    Update src/modules/files/components/create-document/index.tsx
    Co-authored-by: Joseph Silva <josephsilvacasas0121@gmail.com>

--- src/modules/files/components/create-document/index.tsx
+++ src/modules/files/components/create-document/index.tsx
@@ -51,7 +51,6 @@ export function CreateDocumentModal(props: CreateDocumentModalProps) {
             data.append("files", file)
         }
 
-        console.log(data, formData)
 
         dispatch(data)
         router.refresh();
