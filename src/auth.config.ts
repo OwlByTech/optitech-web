a7752a9a0c3ecd0502cd28225bef0b026775e89b
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-07-24 20:07:18 -0500

    fix: correction of redirect to LogIn

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,27 +2,30 @@ import { NextAuthConfig } from "next-auth";
 import { ROUTES_AUTH } from "./modules/auth/types/auth";
 import { ROUTES_SIDEBAR } from "./modules/dashboard/types";
 
-const isProd = process.env.ENV === 'prod';
+const isProd = process.env.ENV === "prod";
 
 export const authConfig: NextAuthConfig = {
-    pages: {
-        signIn: ROUTES_AUTH.LOGIN,
-        newUser: ROUTES_AUTH.SIGN_UP,
+  pages: {
+    signIn: ROUTES_AUTH.LOGIN,
+    newUser: ROUTES_AUTH.SIGN_UP,
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.token = user.token;
+      }
+      return token;
     },
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.token = user.token;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            session.user.token = token.token as string;
-            return session;
-        },
+    async session({ session, token }) {
+      session.user.token = token.token as string;
+      return session;
     },
-    debug: process.env.NODE_ENV !== "production",
-    basePath: ROUTES_SIDEBAR.DASHBOARD,
-    providers: [],
-    trustHost: isProd ? true : undefined,
+    async redirect() {
+      return ROUTES_SIDEBAR.DASHBOARD;
+    },
+  },
+  debug: process.env.NODE_ENV !== "production",
+  basePath: ROUTES_SIDEBAR.DASHBOARD,
+  providers: [],
+  trustHost: isProd ? true : undefined,
 } satisfies NextAuthConfig;
